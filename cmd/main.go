c633ad2062d4e120dc596838b42b670e10e83cd0
okutsen <okutsen@softserveinc.com>
2022-07-05 17:43:30 +0300

    add main package

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/ITA-Dnipro/Dp-230-Test-LFI/internal/client"
+	"github.com/ITA-Dnipro/Dp-230-Test-LFI/internal/lfiscanner"
+)
+
+const (
+	DirLevelUpAttempts = 10
+)
+
+var Targets = map[string]string{
+	"/etc/passwd": "root:x",
+}
+
 func main() {
-	return
+	cl := client.New()
+	lfiScanner := lfiscanner.New(&lfiscanner.Config{
+		Targets: Targets,
+		LevelUpAttempts: 10,
+	}, cl)
+	sc := bufio.NewScanner(os.Stdin)
+	for sc.Scan() {
+		results, err := lfiScanner.ScanUrl(sc.Text())
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to scan url: %s\n", err.Error())
+			continue
+		}
+
+		fmt.Fprintln(os.Stdin, results)
+	}
+	if err := sc.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error while scanning input:", err)
+	}
 }
